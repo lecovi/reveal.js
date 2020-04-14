7dd7e90cb157ac494fa77aa38c92aa1d2297e84f
Leandro E. Colombo Viña <lecovi@zenbook>
2020-04-14 01:02:15 -0300

    Updates Python1

--- archivos/keywords.py
+++ archivos/keywords.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python
-
 import keyword
 
 print("Python tiene", len(keyword.kwlist), "palabras reservadas:")
 
 for keyword in keyword.kwlist:
-        print("\t" + keyword)
+        print(keyword)
